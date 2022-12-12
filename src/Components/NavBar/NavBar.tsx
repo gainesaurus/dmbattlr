8c2f9eefeffca6ca38c02a9c5196c5ab8b7372b4
gainey.michael <gainey.michael@gmail.com>
2022-12-12 09:15:11 -0800

    add css modules, color scheme, mat ui

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Castle } from '@mui/icons-material';
+
+import styles from './NavBar.module.css';
 
 const NavBar = () => {
+
+
   return (
-    <div className="navContainer">
+    <div className={styles.navContainer}>
+      <Castle />
     </div>
   )
 }
+
+export default NavBar;
