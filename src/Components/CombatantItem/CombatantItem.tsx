f661d98235c3527c54d34f334e5b806e8b15ddf9
gainey.michael <gainey.michael@gmail.com>
2022-12-13 16:57:42 -0800

    feat: add expanding divs and mapped combatants

---
+++ src/Components/CombatantItem/CombatantItem.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+import styles from './CombatantItem.module.css';
+
+
+const CombatantItem = ({char}:any) => {
+  const [clicked, setClicked] = useState(false);
+
+  if(!clicked) {
+    return (
+      <div className={styles.combatantContainer} onClick={() => setClicked(true)}>
+        <img 
+          className={styles.smallImg}
+          alt='character' 
+          src={char.img}
+        />
+        <p className={styles.name}>{char.name}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div className={styles.expandContainer} onClick={() => setClicked(false)}>
+        <div className={styles.charCard}>
+          <img 
+            className={styles.expandImg}
+            alt='character' 
+            src={char.img}
+          />
+          <div className={styles.charInfo}>
+            <h3 className={styles.charName}>{char.name}</h3>
+            <h4 className={styles.charBg}>{char.race} {char.background}</h4>
+            <h4 className={styles.charClass}>Level {char.level} - {char.class}</h4>
+            <h5 className={styles.alignment}>{char.alignment}</h5>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CombatantItem;
--- src/Components/CombatantList/CombatantList.tsx
+++ src/Components/CombatantList/CombatantList.tsx
@@ -1,52 +1,54 @@
 import React from 'react';
 import { PersonAdd } from '@mui/icons-material';
+import CombatantItem from '../CombatantItem/CombatantItem';
 
 import styles from './CombatantList.module.css';
+
+//mock data
+const mockPcs = [
+  {
+    name: 'Lady Bard',
+    race: 'Half-Elf',
+    background: 'Charlatan',
+    img: 'https://i0.wp.com/dmdave.com/wp-content/uploads/2019/06/bard-dnd-psionics-3939027972-1561269692278.jpg?fit=638%2C358&ssl=1',
+    level: '8',
+    class: 'Lore Bard',
+    alignment: 'chaotic good',
+  },
+  {
+    name: 'Grimgar the Strange',
+    race: 'Human',
+    background: 'Entertainer',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNOSPSq2NnB1EgsSSpsQyP0T59xvNISANu6w&usqp=CAU',
+    level: '8',
+    class: 'Undead Warlock',
+    alignment: 'chaotic neutral',
+  },
+  {
+    name: 'Gemgold',
+    race: 'Halfling',
+    background: 'Thief',
+    img: 'https://w0.peakpx.com/wallpaper/320/663/HD-wallpaper-late-for-work-today-artist-artwork-digital-art-artstation.jpg',
+    level: '8',
+    class: 'Rogue',
+    alignment: 'chaotic good',
+  },
+  {
+    name: 'Heiderfleiderflaus',
+    race: 'Wood Elf',
+    background: 'Hermit',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRD80iVz0Og2e8DG3bFpVtnUNLXKnlIG4OLQ&usqp=CAU',
+    level: '8',
+    class: 'Circle of the Moon Druid',
+    alignment: 'chaotic good',
+  },
+];
 
 const CombatantList = () => {
 
   return (
     <div className={styles.listContainer}>
-      <div className={styles.combatantContainer}>
-        <img 
-          className={styles.smallImg}
-          alt='character' 
-          src='https://i0.wp.com/dmdave.com/wp-content/uploads/2019/06/bard-dnd-psionics-3939027972-1561269692278.jpg?fit=638%2C358&ssl=1'
-        />
-      </div>
-      <div className={styles.expandContainer}>
-        <img 
-          className={styles.expandImg}
-          alt='character' 
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNOSPSq2NnB1EgsSSpsQyP0T59xvNISANu6w&usqp=CAU'
-        />
-        <div className={styles.charInfo}>
-          <h3 className={styles.charName}>Grimgar the Strange</h3>
-          <h4 className={styles.charBg}>Human Entertainer</h4>
-          <h4 className={styles.charClass}>Level 8 - Undead Warlock</h4>
-        </div>
-      </div>
-      <div className={styles.combatantContainer}>
-        <img 
-          className={styles.smallImg}
-          alt='character' 
-          src='https://w0.peakpx.com/wallpaper/320/663/HD-wallpaper-late-for-work-today-artist-artwork-digital-art-artstation.jpg'
-        />
-      </div>
-      <div className={styles.combatantContainer}>
-        <img 
-          className={styles.smallImg}
-          alt='character' 
-          src='https://i.pinimg.com/originals/32/31/9d/32319d699b3fec0cfc1bcfcd3b367066.jpg'
-        />
-      </div>
-      <div className={styles.combatantContainer}>
-        <img 
-          className={styles.smallImg}
-          alt='character' 
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRD80iVz0Og2e8DG3bFpVtnUNLXKnlIG4OLQ&usqp=CAU'
-        />
-      </div>
+      {mockPcs.map(mock => <CombatantItem char={mock}/>)}
       <div className={styles.addCombatant}>
         <PersonAdd />
       </div>
