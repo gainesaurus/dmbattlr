b6994a369f76cee568b7419de283a6c6d6b86df3
gainey.michael <gainey.michael@gmail.com>
2022-12-31 16:48:36 -0800

    icon styling in stats

--- src/Components/CombatPage/Combatants/CombatantItem/CombatantItem.tsx
+++ src/Components/CombatPage/Combatants/CombatantItem/CombatantItem.tsx
@@ -4,11 +4,17 @@ import BioInfo from './BioInfo/BioInfo';
 import Stats from './Stats/Stats';
 import WeaponList from './Weapons/WeaponList';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faS, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
 import styles from './CombatantItem.module.css';
 
 
 const CombatantItem = ({char}:any) => {
   const [clicked, setClicked] = useState(false);
+
+  library.add(faS, faHeart);
 
   if(!clicked) {
     return (
@@ -28,10 +34,10 @@ const CombatantItem = ({char}:any) => {
         <div className={styles.achp}>
           <h2 className={styles.ac}>{char.armorClass}</h2>
           <div className={styles.hpBox}>
+            <FontAwesomeIcon icon={["fas", "heart"]} />
             <div className={styles.hp}>
-              <h3 className={styles.hpNums}>{char.currentHP}</h3>
-              <h3> / </h3>
-              <h3 className={styles.hpNums}>{char.maxHP}</h3>
+              <h3 className={styles.hpCurrent}>{char.currentHP}/ </h3>
+              <h3 className={styles.hpTotal}>{char.maxHP}</h3>
             </div>
           </div>
           <div className={styles.conditions}>
