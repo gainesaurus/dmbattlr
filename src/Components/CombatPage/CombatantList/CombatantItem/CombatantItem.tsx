7fed4204305b5116b27014dc059f6a25ad1f10a8
gainey.michael <gainey.michael@gmail.com>
2023-01-08 20:11:09 -0800

    add initiative, add stat titles on hover

--- src/Components/CombatPage/CombatantList/CombatantItem/CombatantItem.tsx
+++ src/Components/CombatPage/CombatantList/CombatantItem/CombatantItem.tsx
@@ -5,7 +5,7 @@ import Stats from './Stats/Stats';
 import WeaponList from './WeaponList/WeaponList';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faS, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faS, faHeart, faDiamond } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { RunCircle } from '@mui/icons-material';
 
@@ -16,7 +16,7 @@ import ConditionList from './ConditionList/ConditionList';
 const CombatantItem = ({char}:any) => {
   const [clicked, setClicked] = useState(false);
 
-  library.add(faS, faHeart);
+  library.add(faS, faHeart, faDiamond);
 
   if(!clicked) {
     return (
@@ -34,17 +34,21 @@ const CombatantItem = ({char}:any) => {
       <div className={styles.expandContainer} onClick={() => setClicked(false)}>
         <BioInfo char={char}/>
         <div className={styles.achp}>
-          <h2 className={styles.ac}>{char.armorClass}</h2>
-          <div className={styles.hpBox}>
+          <h2 className={styles.ac} title="armor class">{char.armorClass}</h2>
+          <div className={styles.hpBox} title="health points">
             <FontAwesomeIcon icon={["fas", "heart"]} />
             <div className={styles.hp}>
               <h3 className={styles.hpCurrent}>{char.currentHP}/ </h3>
               <h3 className={styles.hpTotal}>{char.maxHP}</h3>
             </div>
           </div>
-          <div className={styles.speedBox}>
+          <div className={styles.speedBox} title="speed">
             <RunCircle className={styles.speedIcon} fontSize='inherit'/>
             <h3 className={styles.speed}>{char.speed}</h3>
+          </div>
+          <div className={styles.initBox}>
+            <FontAwesomeIcon icon={["fas", "diamond"]} />
+            <h3 className={styles.init} title="initiative">{char.initiative}</h3>
           </div>
         </div>
         <ConditionList />
--- src/Components/CombatPage/CombatantList/CombatantItem/Stats/Stats.tsx
+++ src/Components/CombatPage/CombatantList/CombatantItem/Stats/Stats.tsx
@@ -12,30 +12,30 @@ const Stats = ({char}:any) => {
     <div className={styles.container}>
       <h4 className={styles.title}>Ability Scores:</h4>
       <div className={styles.scoreList}>
-        <div  className={styles.icon}>
+        <div  className={styles.icon} title="strength">
           <FontAwesomeIcon icon={["fas", "hand-fist"]}/>
             <p className={styles.score}>{char.strength}</p>
         </div>
-        <div className={styles.icon}>
+        <div className={styles.icon} title="dexterity">
           <FontAwesomeIcon icon={["fas", "person-falling-burst"]}/>
             <p className={styles.score}>{char.dexterity}</p>
         </div>
-        <div className={styles.icon}>
+        <div className={styles.icon} title="constitution">
           <FontAwesomeIcon icon={["fas", "building-columns"]}/>
             <p className={styles.score}>{char.constitution}</p>
         </div>
       </div>
 
       <div className={styles.scoreList}>
-        <div className={styles.icon}>
+        <div className={styles.icon} title="intelligence">
           <FontAwesomeIcon icon={["fas", "brain"]} className={styles.icon}/>
           <p className={styles.score}>{char.intelligence}</p>
         </div>
-        <div className={styles.icon}>
+        <div className={styles.icon} title="wisdom">
           <FontAwesomeIcon icon={["fas", "book-bookmark"]} className={styles.icon}/>
           <p className={styles.score}>{char.wisdom}</p>
         </div>
-        <div className={styles.icon}>
+        <div className={styles.icon} title="charisma">
           <FontAwesomeIcon icon={["fas", "masks-theater"]} className={styles.icon}/>
           <p className={styles.score}>{char.charisma}</p>
         </div>
