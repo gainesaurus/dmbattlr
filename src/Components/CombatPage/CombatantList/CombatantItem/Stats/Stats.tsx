c801c11628ffb5929191593f95add60be4db3809
gainey.michael <gainey.michael@gmail.com>
2023-01-16 17:07:06 -0800

    add colors to Ability Score styling

--- src/Components/CombatPage/CombatantList/CombatantItem/Stats/Stats.tsx
+++ src/Components/CombatPage/CombatantList/CombatantItem/Stats/Stats.tsx
@@ -4,6 +4,7 @@ import { faS, faHandFist, faPersonFallingBurst, faBuildingColumns, faBrain, faMa
 
 import styles from './Stats.module.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import { pink } from '@mui/material/colors';
 
 const Stats = ({char}:any) => {
   library.add(faS, faHandFist, faPersonFallingBurst, faBuildingColumns, faBrain, faMasksTheater, faBookBookmark);
@@ -13,30 +14,30 @@ const Stats = ({char}:any) => {
       <h4 className={styles.title}>Ability Scores:</h4>
       <div className={styles.scoreList}>
         <div  className={styles.icon} title="strength">
-          <FontAwesomeIcon icon={["fas", "hand-fist"]}/>
-            <p className={styles.score}>{char.strength}</p>
+          <FontAwesomeIcon icon={["fas", "hand-fist"]} style={{color: 'rgba(255,100,100,0.8)'}}/>
+          <p className={styles.score}>{char.strength}</p>
         </div>
         <div className={styles.icon} title="dexterity">
-          <FontAwesomeIcon icon={["fas", "person-falling-burst"]}/>
-            <p className={styles.score}>{char.dexterity}</p>
+          <FontAwesomeIcon icon={["fas", "person-falling-burst"]} style={{color: 'rgba(255, 149, 80,0.8)'}}/>
+          <p className={styles.score}>{char.dexterity}</p>
         </div>
         <div className={styles.icon} title="constitution">
-          <FontAwesomeIcon icon={["fas", "building-columns"]}/>
-            <p className={styles.score}>{char.constitution}</p>
+          <FontAwesomeIcon icon={["fas", "building-columns"]} style={{color: 'rgba(100,150,250,0.8'}}/>
+          <p className={styles.score}>{char.constitution}</p>
         </div>
       </div>
 
       <div className={styles.scoreList}>
-        <div className={styles.icon} title="intelligence">
-          <FontAwesomeIcon icon={["fas", "brain"]} className={styles.icon}/>
+        <div className={styles.icon} title="intelligence" >
+          <FontAwesomeIcon icon={["fas", "brain"]} style={{color: 'rgba(244, 149, 211, 0.8)'}}/>
           <p className={styles.score}>{char.intelligence}</p>
         </div>
         <div className={styles.icon} title="wisdom">
-          <FontAwesomeIcon icon={["fas", "book-bookmark"]} className={styles.icon}/>
+          <FontAwesomeIcon icon={["fas", "book-bookmark"]} style={{color: 'rgba(95, 200, 100, 0.8)'}}/>
           <p className={styles.score}>{char.wisdom}</p>
         </div>
         <div className={styles.icon} title="charisma">
-          <FontAwesomeIcon icon={["fas", "masks-theater"]} className={styles.icon}/>
+          <FontAwesomeIcon icon={["fas", "masks-theater"]} style={{color: 'rgba(190, 120, 250, 0.8)'}}/>
           <p className={styles.score}>{char.charisma}</p>
         </div>
       </div>
